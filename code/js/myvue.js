71a3e9ce2baecf790822aa14bab796fa74338a08
FranklinThree <1765370549@qq.com>
2023-05-07 15:05:09 +0800

    P20 020-手写Vue框架数据代理的实现

---
+++ code/js/myvue.js
@@ -0,0 +1,21 @@
+// 定义一个Vue类
+class Vue{
+    // 定义构造函数
+    // options是一个简单的纯粹的JS对象:{}
+    // options对象中有一个data配置项
+    constructor(options){
+        // 获取所有的属性名
+        Object.keys(options.data).forEach((propertyName, index) => {
+            // console.log(typeof propertyName, propertyName, index)
+            Object.defineProperty(this, propertyName, {
+                get(){
+                    return options.data[propertyName]
+                },
+                set(value){
+                    options.data[propertyName] = value
+                },
+            })
+
+        })
+    }
+}
