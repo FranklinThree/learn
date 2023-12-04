74b3b5b6754f29cc5b25cf48fd1ed4879f6b5214
FranklinThree <1765370549@qq.com>
2023-12-05 01:14:00 +0800

    P52 052-响应式与数据劫持

--- code/js/myvue.js
+++ code/js/myvue.js
@@ -4,27 +4,40 @@ class Vue{
     // options是一个简单的纯粹的JS对象:{}
     // options对象中有一个data配置项
     constructor(options){
-        // 获取所有的属性名
-        Object.keys(options.data).forEach((propertyName, index) => {
-            // console.log(typeof propertyName, propertyName, index)
-            let prefix = propertyName.charAt(0)
-            if(prefix != '_' && prefix != '$'){
-                Object.defineProperty(this, propertyName, {
-                    get(){
-                        return options.data[propertyName]
-                    },
-                    set(val){
-                        options.data[propertyName] = val
-                    },
-                })
-            }
+        if (!(options.data === undefined || options.data === null)){
 
-        })
-        // 获取所有的方法名
-        Object.keys(options.methods).forEach((methodName, index) => {
-            // console.log(methodName);
-            // 给当前的Vue实例拓展一个方法
-            this[methodName] = options.methods[methodName]
-        })
+            // 获取所有的属性名
+            Object.keys(options.data).forEach((propertyName, index) => {
+                // console.log(typeof propertyName, propertyName, index)
+                let prefix = propertyName.charAt(0)
+                if (prefix != '_' && prefix != '$') {
+                    Object.defineProperty(this, propertyName, {
+
+                        // 数据代理
+                        get() {
+                            return options.data[propertyName]
+                        },
+
+                        // 数据劫持
+                        set(val) {
+                            // 1. 修改内存中该对象的属性值。
+                            options.data[propertyName] = val
+                            // 2. 重新渲染页面
+                            console.log('页面上的真实DOM元素更新了！！！')
+                        },
+                    })
+                }
+
+            })
+        }
+        if (!(options.methods === undefined || options.methods === null)){
+            // 获取所有的方法名
+            Object.keys(options.methods).forEach((methodName, index) => {
+                // console.log(methodName);
+                // 给当前的Vue实例拓展一个方法
+                this[methodName] = options.methods[methodName]
+            })
+        }
+
     }
 }
