263c4a6ad5d08849f5945faa659ac3390be1c3d8
FranklinThree <1765370549@qq.com>
2024-02-11 00:03:53 +0800

    P95 095-消息的订阅与发布机制

--- projects/vue_pro/11-BugList综合案例-全局事件总线/main.js
+++ projects/vue_pro/11-BugList综合案例-全局事件总线/main.js
@@ -11,9 +11,9 @@ new Vue({
   el: '#app',
 
   render:h => h(App),
-  // beforeCreate(){
-  //   // 安装全局事件总线
-  //   Vue.prototype.$bus = this
-  // }
+  beforeCreate(){
+    // 安装全局事件总线
+    Vue.prototype.$bus = this
+  }
 
 })
---
+++ projects/vue_pro/src/components/B.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <button @click="publishEnglish">发布2023年第5期英语周报</button>
+  </div>
+</template>
+
+<script>
+import PubSub from 'pubsub-js';
+
+export default{
+  name : 'B',
+  methods : {
+    publishEnglish(){
+      // 发布消息
+      PubSub.publish('2023年第5期英语周报','这是第5期英语周报的详细内容，你感觉怎么样啊？')
+    }
+  }
+}
+</script>
